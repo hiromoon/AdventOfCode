60f6564d45dd8f017e4a46efb71780fa20fc0399
Hiromu Yoshiwara <hiromoon0428@gmail.com>
2018-12-01 21:33:33 +0900

    part2の回答を追加

--- golang/day1.go
+++ golang/day1.go
@@ -9,6 +9,11 @@ import (
 
 func main() {
 	input := readInput()
+	fmt.Println("Part1: ", part1(input))
+	fmt.Println("Part2: ", part2(input))
+}
+
+func part1(input []string) int {
 	var ans int
 	for _, v := range input {
 		n, err := strconv.Atoi(v)
@@ -17,7 +22,58 @@ func main() {
 		}
 		ans += n
 	}
-	fmt.Println(ans)
+	return ans
+}
+
+func part2(input []string) int {
+	is := convertToIntList(input)
+
+	found := false
+	var ans int
+	t := []int{0}
+	for !found {
+		t = calcFrequencyList(t, is)
+		found, ans = getFirstDuplicate(t)
+	}
+	return ans
+}
+
+func calcFrequencyList(ans []int, input []int) []int {
+	last := ans[len(ans)-1]
+	for _, v := range input {
+		last += v
+		ans = append(ans, last)
+	}
+	return ans
+}
+
+func convertToIntList(input []string) []int {
+	var res []int
+	for _, v := range input {
+		n, err := strconv.Atoi(v)
+		if err != nil {
+			panic(err.Error())
+		}
+		res = append(res, n)
+	}
+	return res
+}
+
+func getFirstDuplicate(input []int) (bool, int) {
+	var ans int
+	encountered := map[int]bool{}
+	found := false
+	for _, v := range input {
+		if !encountered[v] {
+			encountered[v] = true
+		} else {
+			ans = v
+			found = true
+			break
+		}
+	}
+
+	return found, ans
 }
 
 func readInput() []string {
